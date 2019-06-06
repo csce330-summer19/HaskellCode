ac2d0bf91f05da2f5ba8bbd4b4ef65a6bfc5d4a1
James O'Reilly <oreillyj@cse.sc.edu>
2019-06-06 10:13:28 -0400

    6/5

---
+++ ch6.hs
@@ -0,0 +1,35 @@
+and' :: [Bool] -> Bool
+--and' [] = True
+--and' (False:_) = False
+--and' (True:xs) = and' xs
+
+and' [] =True
+and' (bool:bools) = bool && and' bools
+
+concat' :: [[a]] -> [a]
+concat' [] = []
+concat' (xs:xss) = xs ++ concat' xss 
+
+replicate' :: Int -> a -> [a]
+replicate' 0 _ = []
+--replicate' n x = [x] ++ replicate' (n-1) x
+replicate' n x = x : replicate' (n-1) x
+
+ind :: [a] -> Int -> a
+ind (x:_) 0 = x
+ind (_:xs) i = ind xs (i-1) 
+
+elem' :: Eq a => a -> [a] -> Bool
+elem' _ [] = False
+elem' x (y:ys) 
+   | x==y = True
+   | otherwise = elem' x ys
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] [] = []
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) 
+   | x <= y    = x : merge xs (y:ys)
+   | otherwise = y : merge (x:xs) ys
+
